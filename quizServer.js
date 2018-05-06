91d815978be78adacae5ee020f4e00623eb0c354
studentuser <uceskai@ucl.ac.uk>
2018-05-06 13:36:24 -0400

    added function for alert showing the user form input

--- quizServer.js
+++ quizServer.js
@@ -38,27 +38,82 @@ var pg = require('pg');
 var pool = new pg.Pool(config);
 
 // test the connection:		(http://developer.cege.ucl.ac.uk:31099/postgistest)
-	app.get('/postgistest', function (req,res) {
-		console.log('postgistest');
-		pool.connect(function(err,client,done) {
-		if(err){
-				   console.log("not able to get connection "+ err);
+app.get('/postgistest', function (req,res) {
+	console.log('postgistest');
+	pool.connect(function(err,client,done) {
+	if(err){
+			   console.log("not able to get connection "+ err);
+			   res.status(400).send(err);
+		   } 
+			client.query('SELECT name FROM united_kingdom_counties' ,function(err,result) {
+			console.log("query");
+			   done(); 
+			   if(err){
+				   console.log(err);
 				   res.status(400).send(err);
-			   } 
-				client.query('SELECT name FROM united_kingdom_counties' ,function(err,result) {
-				console.log("query");
-				   done(); 
-				   if(err){
-					   console.log(err);
-					   res.status(400).send(err);
-				   }
-				   res.status(200).send(result.rows);
-			   });
-			});
-	});
+			   }
+			   res.status(200).send(result.rows);
+		   });
+		});
+});
 
+	
+	
 // DATA UPLOAD: QUESTIONS
+//function for upoad start alert
 function startDataUpload() {
 alert ("start data upload");
+}
+
+// add body-parser to enable the processing of the uploaded data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+ extended: true
+}));
+app.use(bodyParser.json());
+
+//add post request so the form data is returned to the userAgent
+app.post('/uploadData',function(req,res){
+// use POST as we are uploading data
+// --> parameters form part of the BODY of the request rather than the RESTful API
+console.dir(req.body);
+// echo the request back to the user
+res.send(req.body);
+});
+
+// add AJAX call & response method
+// function to process the input data 
+var client;
+function processData(postString) {
+ client = new XMLHttpRequest();
+ client.open('POST','http://developer.cege.ucl.ac.uk:30299/uploadData',true);
+ client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+ client.onreadystatechange = dataUploaded;
+ client.send(postString);
+}
+// function to wait for server response (ready = state 4) and for response processing
+// will print the response text into the "data Upload Result" div
+function dataUploaded() {
+ if (client.readyState == 4) {
+ // change the DIV to show the response
+ document.getElementById("dataUploadResult").innerHTML = client.responseText;
+ }
+}
+
+// upload form data and create alert with form input
+function startDataUpload() {
+	alert ("start data upload");
+	
+	var loc = document.getElementById("location").value;
+	var question = document.getElementById("question").value;
+	var correct = document.getElementById("correct").value;
+	var answer1 = document.getElementById("answer1").value;
+	var answer2 = document.getElementById("answer2").value;
+	var answer3 = document.getElementById("answer3").value;
+	
+	var postString = "location="+loc+"&question"+question+"&correct"+correct+"&answer1"+answer1+"&answer2"+answer2+"&answer3"+answer3;
+	processData(postString);
+	
+	alert(loc + " "+ question + " "+ correct + " "+ answer1 + " "+ answer2 + " "+ answer3);
 }
 
