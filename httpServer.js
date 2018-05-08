b9f36ceea7e02d8a9f09171f0630dff8271a2853
mkainz <uceskai@ucl.ac.uk>
2018-05-08 13:24:43 -0400

    added question display in quiz app

--- httpServer.js
+++ httpServer.js
@@ -124,6 +124,87 @@ app.get('/getPOI', function (req,res) {
 });
 
 
+
+
+
+//  SUBMIT THE USER REPLY
+app.post('/submitQuestion',function(req,res){
+	// note that we are using POST here as we are uploading data
+	// so the parameters form part of the BODY of the request rather than the RESTful API
+	console.dir(req.body);
+
+ 	pool.connect(function(err,client,done) {
+       	if(err){
+          	console.log("not able to get connection "+ err);
+           	res.status(400).send(err);
+       	} 
+// pull the geometry component together
+// note that well known text requires the points as longitude/latitude !
+// well known text should look like: 'POINT(-71.064544 42.28787)'
+
+var querystring = "INSERT into answers (question, answer) values ('" + req.body.question + ",'" + req.body.answer + "')";
+																									
+       	console.log(querystring);
+       	client.query( querystring,function(err,result) {
+          done(); 
+          if(err){
+               console.log(err);
+               res.status(400).send(err);
+          }
+          res.status(200).send("row inserted");
+       });
+    });
+
+});
+
+
+
+
+
+
+
+
+// get specific columns form the questions database
+app.get('/getPOI2', function (req,res) {
+	pool.connect(function(err,client,done) {
+	if(err){
+		console.log("not able to get connection "+ err);
+		res.status(400).send(err);
+	}
+	client.query('SELECT (location, question, correct, answer1, answer2, answer3) FROM questions',function(err,result) {
+		done();
+		if(err){
+			console.log(err);
+			res.status(400).send(err);
+		}
+		res.status(200).send(result.rows);
+	});
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// other stuff, delete possibly
 app.get('/getGeoJSON/:tablename/:geomcolumn', function (req,res) {
      pool.connect(function(err,client,done) {
       	if(err){
--- uploadData.js
+++ uploadData.js
@@ -1,14 +1,26 @@
 function startDataUpload() {
 	alert ("start data upload");
 
-	var loc = document.getElementById("location").value;
-	var question = document.getElementById("question").value;
+	var question = "'Head Question'";
 	var correct = document.getElementById("correct").value;
 	var answer1 = document.getElementById("answer1").value;
 	var answer2 = document.getElementById("answer2").value;
 	var answer3 = document.getElementById("answer3").value;
 	
-	var postString = "location="+loc +"&question="+question + "&correct="+correct +"&answer1="+answer1 +"&answer2="+answer2 +"&answer3="+answer3;
+	var postString = "&question= Whose head in main cloisters?"+question + "&correct="+correct +"&answer1="+answer1 +"&answer2="+answer2 +"&answer3="+answer3;
+	//get answer (radio buttons)
+	if (document.getElementById("answer1").checked) {
+		postString=postString+"&answer=Jeremy Bentham";
+	}
+	if (document.getElementById("answer2").checked) {
+		postString=postString+"&answer=Elton John";
+	}	
+	if (document.getElementById("answer3").checked) {
+		postString=postString+"&answer=A prehistoric opposum";
+	}	
+	if (document.getElementById("answer4").checked) {
+		postString=postString+"&answer=Joe Tribbiani";
+	}	
 	processData(postString);
 	
 	alert (postString);
@@ -17,7 +29,7 @@ function startDataUpload() {
 var client;
 function processData(postString) {
 	client = new XMLHttpRequest();
-	client.open('POST','http://developer.cege.ucl.ac.uk:30299/uploadData',true);
+	client.open('POST','http://developer.cege.ucl.ac.uk:30299/submitQuestion',true);
 	client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	client.onreadystatechange = dataUploaded;
 	client.send(postString);
